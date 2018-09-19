23d213822c6d7a2d9622eb328a543d3184012aaa
KryslanGomes <39163187+KryslanGomes@users.noreply.github.com>
2018-09-19 11:01:59 -0300

    Layout melhorado.

--- app/src/main/java/com/gomes/kryslan/alarmelembrete/View/TelaDespertador.java
+++ app/src/main/java/com/gomes/kryslan/alarmelembrete/View/TelaDespertador.java
@@ -9,6 +9,11 @@ import android.widget.TextView;
 import com.gomes.kryslan.alarmelembrete.Controller.Tools;
 import com.gomes.kryslan.alarmelembrete.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class TelaDespertador extends Tools {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +24,18 @@ public class TelaDespertador extends Tools {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         );
+
+        //SETANDO HORA
+        TextView txHora = findViewById(R.id.hora);
+        TextView txMinuto = findViewById(R.id.minuto);
+
+        Date hoje = Calendar.getInstance().getTime();
+        Locale local = getResources().getConfiguration().locale;  //Pega o local para definir o tempo corretamente baseado no fuso horário.
+        SimpleDateFormat formatoHora = new SimpleDateFormat("HH", local);
+        SimpleDateFormat formatoMinuto = new SimpleDateFormat("mm", local);
+
+        txHora.setText(formatoHora.format(hoje.getTime()));
+        txMinuto.setText(formatoMinuto.format(hoje.getTime()));
 
     }
 }
